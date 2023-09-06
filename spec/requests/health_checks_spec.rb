7d186a551509cf4aa2d92481c2b362a711fa8e23
HidetoshiHibino <109229123+HidetosHibino@users.noreply.github.com>
2023-09-06 09:41:13 +0900

    Default image url (#16)
    * デフォルト画像の変更
    
    * dockerfileの修正

--- spec/requests/health_checks_spec.rb
+++ spec/requests/health_checks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "HealthChecks", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/health_checks/index"
+      get "/health_check"
       expect(response).to have_http_status(:success)
     end
   end
