4eec42ac0a7ac6280281019f1a5ed3b8e12efa81
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-09-12 14:03:52 +0900

    commentable/favoritableにdependent::destroyを付与

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -2,7 +2,7 @@ module Commentable
   extend ActiveSupport::Concern
 
   included do
-    has_many :comments, as: :commentable
+    has_many :comments, as: :commentable, dependent: :destroy
     has_many :commented_by_users, through: :comments, source: :user
   end
 end
--- app/models/concerns/favoritable.rb
+++ app/models/concerns/favoritable.rb
@@ -2,7 +2,7 @@ module Favoritable
   extend ActiveSupport::Concern
 
   included do
-    has_many :favorites, as: :favoritable
+    has_many :favorites, as: :favoritable, dependent: :destroy
     has_many :favorited_by_users, through: :favorites, source: :user
   end
 end
